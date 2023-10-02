7b77a2924022945248f43c2adead312e0d9429fe
Yushi Xing <130510998+xingys0@users.noreply.github.com>
2023-10-01 18:01:15 -0700

    Assignment2:CustomTableViewWithXIB

---
+++ CustomTableViewWithXIB/CustomTableViewWithXIB/ImageTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ImageTableViewCell.swift
+//  CustomTableViewWithXIB
+//
+//  Created by sylvia on 10/1/23.
+//
+
+import UIKit
+
+class ImageTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imgView: UIImageView!
+    
+    @IBOutlet weak var lblImage: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
